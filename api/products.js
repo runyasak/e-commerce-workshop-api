fbfaa84c7a5f7d42dca4672438d59a104d1da3b7
Runyasak Chaengnaimuang <world@odds.team>
2022-11-02 16:31:57 +0700

    feat: add package.json

--- api/products.js
+++ api/products.js
@@ -1,6 +1,6 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch'
 
-export default function handler(request, response) {
+export default function (request, response) {
   fetch('https://fakestoreapi.com/products/1')
     .then(res => res.json())
     .then(response.status(200).json)
